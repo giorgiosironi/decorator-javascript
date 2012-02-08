77e5a59808bb2cead2949f5eaae5b247e0538f9d
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-02-08 09:26:36 +0100

    Specifying a Decorator-based design for blinking and jumping balls

--- test/BallTest.js
+++ test/BallTest.js
@@ -4,17 +4,17 @@ TestCase("basic Ball object test", {
          assertEquals("I am a ball", ball.what());
     },
     "test a blinking ball is a ball that can blink" : function () {
-         var ball = new BlinkingBall();
+         var ball = Blinking.decorate(new Ball());
          assertEquals("I am a ball", ball.what());
          assertTrue(ball.canBlink());
     },
     "test a jumping ball is a ball that can jump" : function () {
-         var ball = new JumpingBall();
+         var ball = Jumping.decorate(new Ball());
          assertEquals("I am a ball", ball.what());
          assertTrue(ball.canJump());
     },
     "test a blinking&jumping ball is a ball that can do both" : function () {
-         var ball = new BlinkingJumpingBall();
+         var ball = Blinking.decorate(JumpingBall.decorate(new Ball()));
          assertEquals("I am a ball", ball.what());
          assertTrue(ball.canBlink());
          assertTrue(ball.canJump());
