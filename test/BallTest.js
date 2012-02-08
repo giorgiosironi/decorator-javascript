1d10b128b86213bf564b97a3f7cdc9c40e553ff9
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-02-08 09:24:21 +0100

    Specifying a design for blinking and jumping balls

--- test/BallTest.js
+++ test/BallTest.js
@@ -2,5 +2,21 @@ TestCase("basic Ball object test", {
     "test it should tell us what it is" : function() {
          var ball = new Ball();
          assertEquals("I am a ball", ball.what());
+    },
+    "test a blinking ball is a ball that can blink" : function () {
+         var ball = new BlinkingBall();
+         assertEquals("I am a ball", ball.what());
+         assertTrue(ball.canBlink());
+    },
+    "test a jumping ball is a ball that can jump" : function () {
+         var ball = new JumpingBall();
+         assertEquals("I am a ball", ball.what());
+         assertTrue(ball.canJump());
+    },
+    "test a blinking&jumping ball is a ball that can do both" : function () {
+         var ball = new BlinkingJumpingBall();
+         assertEquals("I am a ball", ball.what());
+         assertTrue(ball.canBlink());
+         assertTrue(ball.canJump());
     }
 });
